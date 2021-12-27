7a29f96b9d65d451fd51c011eb6b52440133eb53
JungHm <taggma12@gmail.com>
2021-12-28 00:40:08 +0900

    .

--- SwiftAlgorithm/main.swift
+++ SwiftAlgorithm/main.swift
@@ -7,4 +7,18 @@
 
 import Foundation
 
-print("hello")
+
+var input = Int(readLine()!)!
+var answer = 0
+var sum = 0
+
+for item in (input-54)..<input {
+    sum = item + (String(item).map{Int(String($0))!}.reduce(0,+))
+    if sum == input {
+        answer = item
+        break
+    }
+}
+
+print(answer)
+
