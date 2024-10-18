7aaf7388a9c1d82bc91716e2515e79a575ef3d6c
JungHm <taggma12@gmail.com>
2024-10-18 15:36:10 +0900

    [Bronze III] Title: 배수와 약수, Time: 4 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.swift
@@ -0,0 +1,8 @@
+while true {
+    let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+    if input[0] == 0 && input[1] == 0 { break }
+    
+    if (input[1] % input[0] == 0) { print("factor") }
+    else if (input[0] % input[1] == 0) { print("multiple") }
+    else { print("neither") }
+}
